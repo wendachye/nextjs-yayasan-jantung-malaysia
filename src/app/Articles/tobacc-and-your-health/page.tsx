06a86b63e38d5c9b9b3d112546d42239c892047a
WeiLun <lohweilun88@gmail.com>
2024-12-10 16:01:59 +0800

    tobbac article

---
+++ src/app/Articles/tobacc-and-your-health/page.tsx
@@ -0,0 +1,116 @@
+import styles from './styles.module.css';
+
+const HeartAttack = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Heart Attack</h1>
+      <h2 className={styles.subtitle}>What is Heart Attack?</h2>
+      <br />
+      <p className={styles.description}>
+        Heart attack results from blood vessel disease in the heart. Coronary
+        heart disease (CHD), sometimes referred to as coronary after disease
+        (CAD), are general names for heart attack (and angina).
+        <br />
+        <br />
+        A heart attack, or myocardial infarction, occurs when the blood supply
+        to part of the heart muscle itself (the myocardium) is severely reduced
+        or stopped. This occurs when one of the coronary arteries (the arteries
+        that supply blood to the heart muscle) is blocked by an obstruction,
+        such as a blood clot that has formed on plaque due to atherosclerosis.
+        Such an event is sometimes called a coronary thrombosis or coronary
+        occlusion.
+        <br />
+        <br />
+        If the blood supply is cut off drastically or for a long time, muscle
+        cells suffer irreversible injury and die. Disability or death can
+        result, depending on how much heart muscle is damaged. Sometimes a
+        coronary artery temporarily contracts or goes into spasm. When this
+        happens the artery narrows and blood flow to part of the heart muscle
+        decreases or even stops. What causes a spasm is unclear, but it can
+        occur in normal blood vessels as well as vessels partially blocked by
+        atheroscleorosis. If a spasm is severe, a heart attach may result.
+        <br />
+        <br />
+      </p>
+
+      <h2 className={styles.subtitle}>
+        What are the Symptoms of a Heart Attack?
+      </h2>
+      <br />
+      <p className={styles.description}>
+        Sometimes the first indications of a heart attack come as warning
+        signals. The actual diagnosis of a heart attack must be made by a
+        physician who has studied the results of several tests. Besides
+        reviewing a patient's complete medical history and giving a physical
+        examination, a doctor will use an electrocardiogram (ECG) to discover
+        any abnormalities caused by damage to the heart. Sometimes a blood test
+        is used to detect abnormal levels of certain enzymes in the bloodstream.
+        <br />
+        <br />
+      </p>
+
+      <h2 className={styles.subtitle}>How is a Heart Attack Treated?</h2>
+      <br />
+      <p className={styles.description}>
+        When a heart attack occurs, it's critical to recognize the signals and
+        respond immediately. Delaying may increase the damage to the heart and
+        reduce the chance of survival. Anyone experiencing the warning signals
+        of a heart attack should be taken immediately to the nearest hospital
+        with 24-hour emergency cardiac care. People who become unconscious
+        before reaching the emergency room may receive emergency cardiopulmonary
+        resuscitation (CPR).
+        <br />
+        <br />
+        Most communities have an emergency cardiac care system that can quickly
+        respond. This prompt care for heart attack victims dramatically reduces
+        damage to the heart. In fact, 80 percent of heart attack survivors can
+        return to work within three months. Prompt care for heart attack victims
+        isn't the only reason so many people recover so quickly, but it's an
+        important one.
+        <br />
+        <br />
+        The importance of time cannot be overemphasized. When a coronary artery
+        gets blocked, the heart muscle doesn't die instantaneously - damage
+        increases the longer an artery remains blocked. If a victim gets to an
+        emergency room fast enough a form of reperfusion therapy (called
+        thrombolysis) sometimes can be performed. It involves injecting
+        injecting a thrombolytic (clot-dissolving) agent, such as streptokinase,
+        urokinase or TPA (tissue plasminogen activator), to dissolve a clot in a
+        coronary artery and restore some blood flow. There drugs must be used
+        within a few (usually 1 -3) hours of a heart attack for best effect. The
+        sooner a drug is used, the more effective it's likely to be.
+        <br />
+        <br />
+        In the weeks following a heart attack, either PTCA (balloon angioplasty)
+        or coronary artery bypass surgery may be performed to improve the blood
+        supply to the heart muscle. Once part of the heart muscle dies, its
+        function can't be restored. Function may be restored to areas with
+        decreased blood flow, however.
+        <br />
+        <br />
+      </p>
+
+      <h2 className={styles.subtitle}>
+        Is There Any Way to Reduce the Chance of a Heart Attack?
+      </h2>
+      <br />
+      <p className={styles.description}>
+        Many scientific studies show that certain characteristics increase the
+        risk of coronary heart disease. The four major modifiable risk factors
+        are cigarette / tobacco smoke, high blood cholesterol, high blood
+        pressure and physical inactivity. Other contributing risk factors are
+        diabetes mellitus and obesity.
+        <br />
+        <br />
+        The Heart Foundation strongly urges people to control their modifiable
+        risk factors. Also, people with angina should take episodes of chest
+        pain seriously and see their doctor before their atherosclerosis leads
+        to a heart attack.
+        <br />
+        <br />
+      </p>
+    </div>
+  );
+};
+
+export default HeartAttack;
