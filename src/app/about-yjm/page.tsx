2bc9f6412401a1886158f8551be500406f6a66f3
Wenda Chye <wenda0216@gmail.com>
2024-12-10 11:37:11 +0800

    feat: add metadata and about page

---
+++ src/app/about-yjm/page.tsx
@@ -0,0 +1,53 @@
+import styles from './styles.module.css';
+
+const AboutYJM = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>The Heart Foundation of Malaysia</h1>
+      <h2 className={styles.subtitle}>Yayasan Jantung Malaysia (YJM)</h2>
+      <p className={styles.description}>
+        The groundwork for today&apos;s solid base began in 1982. The Heart
+        Foundation of Malaysia was established by a small group of dedicated and
+        concerned lay people who were determined to make an impact on the
+        control of heart and blood vessel disease.
+        <br />
+        <br />
+        In the 1960&apos;s, deaths in Malaysia were due to communicable diseases
+        such as tuberculosis, malaria, typhoid and cholera. Our health system
+        was then developing and our health facilities were inadequate to meet
+        the needs and demands of the population.
+        <br />
+        <br />
+        With the improved standard of living and better understanding, now,
+        there is a control of these diseases. However, more than 20 years later,
+        heart disease death rates and stroke deaths have increased and has
+        become the No. 1 killer. More people are being affected by heart and
+        blood vessel diseases.
+        <br />
+        <br />
+        We believe we can share in the effort to prevent and control the
+        situation. Educating the public by bringing awareness has resulted in
+        encouraging response from the society. Heart and blood vessel disease is
+        our major killer, claiming almost twice as many lives as cancer and
+        accounting for a high percentage of all certified deaths each year. In
+        1995, 28.2 % of all medically reported deaths in Malaysia were
+        attributed to CVD (cardiovascular diseases). By international standards,
+        this figure does cause a concern for Malaysia.
+        <br />
+        <br />
+        <b>
+          The Heart Foundation of Malaysia is a non-profit and non-governmental
+          organisation
+        </b>{' '}
+        dedicated to the education of the public on heart and circulatory
+        diseases. We are funded almost entirely by public support from our
+        well-meaning society. In addition, we receive some limited government
+        co-operation for specific projects. The Foundation&apos;s heart-health
+        message is direct and effective - enjoy healthy eating, exercise, be
+        smoke-free and check your blood pressure regularly.
+      </p>
+    </div>
+  );
+};
+
+export default AboutYJM;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './styles.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <picture className={styles.logo}>
-        <Image fill alt="logo" src="/images/logo.png" sizes="602px" />
-      </picture>
+      <Link href="/">
+        <picture className={styles.logo}>
+          <Image fill alt="logo" src="/images/logo.png" sizes="602px" />
+        </picture>
+      </Link>
 
       <nav className={styles.nav}>
         <a>About YJM</a>
