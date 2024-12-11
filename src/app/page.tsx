1b3e41b204ab1eef511402757f53baf8623b63e4
Wenda Chye <wenda0216@gmail.com>
2024-12-11 16:43:43 +0800

    feat: add home banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './page.module.css';
 
-export default function Home() {
+const URL_ADS = 'https://biogrow.com.my';
+
+const Home = () => {
   return (
-    <div className="">
+    <div>
       <div className={styles.banner}>
-        <Image fill alt="home-banner" src="/images/home_banner.jpg" />
+        <Image fill alt="home-banner" src="/images/home/home_banner.jpg" />
       </div>
+
+      <Link href={URL_ADS} target="_blank">
+        <div className={styles.banner_ads}>
+          <Image
+            fill
+            alt="home-banner"
+            src="/images/home/home_banner_ads.jpg"
+          />
+        </div>
+      </Link>
     </div>
   );
-}
+};
+
+export default Home;
