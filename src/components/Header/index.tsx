bba77465434cf70baa6d4e72ad3b6475f79de6cf
Wenda Chye <wenda0216@gmail.com>
2024-12-14 01:53:52 +0800

    feat: update header menu items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -25,19 +25,141 @@ const menu = [
         title: 'Our Aspirations',
         link: '',
       },
+      {
+        title: 'Our Future',
+        link: '',
+      },
+      {
+        title: 'Our Activities',
+        link: '',
+      },
+      {
+        title: 'Promoting Health',
+        link: '',
+      },
+      {
+        title: 'Research - The Heart of a Revolution',
+        link: '',
+      },
+      {
+        title: 'CPR (Cardio Pulmonary Resuscitation)',
+        link: '',
+      },
+      {
+        title: 'Friends of the Foundation',
+        link: '',
+      },
+      {
+        title: 'How You Can Help',
+        link: '',
+      },
+      {
+        title: 'You Can Make A Difference!',
+        link: '',
+      },
     ],
   },
   {
     title: 'Facilities',
     link: '',
+    items: [
+      {
+        title: 'Hall Rental',
+        link: '',
+      },
+      {
+        title: 'Halfway House',
+        link: '',
+      },
+    ],
   },
   {
     title: 'Health Articles',
     link: '',
+    items: [
+      {
+        title: 'Video: How Does the Human Heart Work?',
+        link: '',
+      },
+      {
+        title: 'Video: Four Ways to Prevent Heart Attack',
+        link: '',
+      },
+      {
+        title: 'Video: Health Matters - Heart Disease Warning',
+        link: '',
+      },
+      {
+        title: 'Video: How Does Exercise Impact Weight Loss',
+        link: '',
+      },
+      {
+        title: 'Heart Basics',
+        link: '',
+      },
+      {
+        title: 'Steps to Better Health',
+        link: '',
+      },
+      {
+        title: 'How Aerobic Exercise Can Speed Up Your Fatloss!',
+        link: '',
+      },
+      {
+        title: 'Break Your Bad Habits',
+        link: '',
+      },
+      {
+        title: 'Tobacco and Your Health',
+        link: '',
+      },
+      {
+        title: 'Heart Attack',
+        link: '',
+      },
+      {
+        title: "Heart Disease - Not somebody else's business",
+        link: '',
+      },
+      {
+        title: 'Arrhythmias and Sudden Cardiac Death',
+        link: '',
+      },
+      {
+        title: 'Inherited Cholesterol Disorder',
+        link: '',
+      },
+    ],
   },
   {
     title: 'Activities',
     link: '',
+    items: [
+      {
+        title: 'Photo Gallery & Albums',
+        link: '',
+      },
+      {
+        title: 'CPR (Cardio Pulmonary Resuscitation)',
+        link: '',
+      },
+      {
+        title: 'AXA Hearts in Action Run 2020',
+        link: '',
+      },
+      {
+        title: 'Walk A Million Miles 2020',
+        link: '',
+      },
+      {
+        title: 'World Heart Day',
+        link: '',
+      },
+      {
+        title: 'Go Red for Women',
+        link: '',
+      },
+    ],
   },
   {
     title: 'Bulletin',
@@ -46,6 +168,24 @@ const menu = [
   {
     title: 'Contact Us',
     link: '',
+    items: [
+      {
+        title: 'Contact Us',
+        link: '',
+      },
+      {
+        title: 'Membership',
+        link: '',
+      },
+      {
+        title: 'Donation',
+        link: '',
+      },
+      {
+        title: 'Vacancy',
+        link: '',
+      },
+    ],
   },
 ];
 
